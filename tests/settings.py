813739f495c63d01098415b59533043fab88baec
Jakub <jakub@opportunitynetwork.com>
2017-03-16 12:10:08 +0100

    Adding a middleware that ensures api key authentication.

--- tests/settings.py
+++ tests/settings.py
@@ -33,7 +33,7 @@ REST_FRAMEWORK = {
     )
 }
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
