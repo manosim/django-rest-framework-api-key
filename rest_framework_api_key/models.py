bffdf012cfce8006d11d7dec860dd9055dd90547
Dryice Liu <dryiceliu@gmail.com>
2018-03-20 15:34:59 +0800

    a bit doc

--- rest_framework_api_key/models.py
+++ rest_framework_api_key/models.py
@@ -15,7 +15,7 @@ class APIKey(models.Model):
 
     name = models.CharField(max_length=50, unique=True)
     key = models.CharField(max_length=40, unique=True)
-    path_re = models.CharField(max_length=1024)
+    path_re = models.CharField(max_length=1024) # an RE that this api-key is validate for
 
     def __str__(self):
         return self.name
